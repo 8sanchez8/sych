6c871e42204be42240183e446d014f2266497176
8sanchez8 <8sanchez8@gmail.com>
2016-05-13 22:47:46 +0300

    Update settings.py

--- sych_server/settings.py
+++ sych_server/settings.py
@@ -61,7 +61,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            '',
+            os.path.join(BASE_DIR).replace('\\','/'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
